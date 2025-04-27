1f3a4ea41b5a5f8aa1a4c84773e1907db17e0818
TelonaHyugaRinne <163213533+TelonaHyuga@users.noreply.github.com>
2025-04-27 07:00:21 +0300

    Add files via upload

---
+++ assets/php/lang.php
@@ -0,0 +1,216 @@
+<?php
+    // Language translations
+    $translations = [
+    'ru' => [
+        'theme' => 'Смена темы',
+        'lang'=> 'Язык',
+        'info'=> 'Информация',
+        'partfel'=> 'Партфолио',
+        'partner'=> 'Партнёры',
+        'home'=> 'Главная',
+        'links'=> 'Ссылки',
+        'hua-chen'=> 'Хуа-Чэн - благословение небожителей',
+        'bay-usen'=> 'Бай-Усян - благословение небожителей',
+        'THR-Mask'=> '(отображается с некритичными ошибками)',
+        'person1'=> '<h2 class="TTBT">
+                         Тэлона Хьюга Риннэ
+                    </h2><br>
+                    <h3 class="TTBT">
+                         - Контент мейкерша по игре minecraft, программистка, художница и просто любительница технологий
+                    </h3><br>
+                    <h3 class="TTBT">
+                        <cite>
+                         - "Никогда не оставляю, что то на долго, вещи нужны, что бы их обновлять"
+                        </cite>
+                    </h3><br>
+                        <h3 class="TTBT">
+                         - Увлекается созданием механизмов на редстоуне [Транс-фем <img src="./assets/image/transgender.svg" class="img_1_1" style="width: 1.2vw;">]
+                    </h3>',
+        'Partner1'=> '<h1 class="TTBT">
+                             Minneria-Hyuga
+                        </h1><br>
+                        <h3 class="TTBT">
+                             - Союз "Империи" и "клана", создающие вместе контент для игры minecraft
+                        </h3><br>
+                        <h3 class="TTBT">
+                             - Логотип символизирует сильное доверие сторон друг к другу
+                        </h3>',
+        'Partner2'=> '<h1 class="TTBT">
+                          GSMP
+                    </h1><br>
+                    <h3 class="TTBT">
+                          - Gatherings minecraft smp
+                    </h3><br>
+                    <h3 class="TTBT">
+                         - Сервер друзей, создающих контент в игре и просто проводят вместе время
+                    </h3>',
+        'post1' => '<h1 class="TTBT">
+                        Привет!
+                    </h1><br>
+                    <h2 class="TTBT">
+                         - Ты зашёл на мой сайт, а значит я тебя чем то заинтересовала)</h2>
+                    <br><h2 class="TTBT">
+                         - Тут ты можешь посмотреть мои работы, узнать мои контакты или информацию о других интересных людях/компаниях,
+                         с которыми я сотрудничаю или в которых состою!</h2>',
+        'post2'=> '<h1 class="TTBT">
+                        Сейчас я работаю над созданием плеера
+                    </h1><br>
+                    <h2 class="TTBT">
+                        - Я не нашла ни одного хорошего плеера, который и работал с функциями операционок, по типу сетки окон на windows 11,
+                         и был функциональный, и настраиваемый и пр., по этому я решила, что хочу создать, что то своё
+                    </h2><br>
+                    <h2 class="TTBT">
+                        - У меня много идей, но я вэб фронтэндерша и дизайнерша, а этого очевидно мало из-за чего у меня есть определённые
+                         трудности в реализации и скорее всего это затянется на долго
+                    </h2><br>
+                    <h2 class="TTBT">
+                        - Это не коммерческий проект, как только плеер будет закончен, я открою доступ на скачивание на этом сайте в соответствующей вкладке
+                    </h2><br>
+                    <h2 class="TTBT">
+                        - Если ты обладаешь желанием и навыками программирования, я буду рада любой помощи, напиши мне в тг, может мы найдём общий язык
+                         и вместе сможем быстрее реализовать один из лучших в мире плееров!
+                    </h2>',
+        ],
+    'en' => [
+        'theme' => 'Theme Switch',
+        'lang'=> 'Language',
+        'info'=> 'Information',
+        'partfel'=> 'Portfolio',
+        'partner'=> 'Partner',
+        'home'=> 'Home',
+        'links'=> 'Links',
+        'hua-chen'=> 'Hua Cheng - Blessing of the Celestials',
+        'bay-usen'=> 'Bai-Wuxiang - blessing of the celestials',
+        'THR-Mask'=> '(displayed with non-critical errors)',
+        'person1'=> '<h2 class="TTBT">
+                         Telona Hyuga Rinne
+                    </h2><br>
+                    <h3 class="TTBT">
+                         - Minecraft content maker, programmer, artist and just a technology lover
+                    </h3><br>
+                    <h3 class="TTBT">
+                        <cite>
+                         - "I never leave anything for a long time, things need to be updated"
+                        </cite>
+                    </h3><br>
+                        <h3 class="TTBT">
+                         - Enjoys creating mechanisms using redstone [Trans-fem <img src="./assets/image/transgender.svg" class="img_1_1" style="width: 1.2vw;">]
+                    </h3>',
+        'Partner1'=> '<h1 class="TTBT">
+                             Minneria-Hyuga
+                        </h1><br>
+                        <h3 class="TTBT">
+                             - The union of "Empire" and "clan" creating content for the game minecraft together
+                        </h3><br>
+                        <h3 class="TTBT">
+                             - The logo symbolizes the strong trust between the parties
+                        </h3>',
+        'Partner2'=> '<h1 class="TTBT">
+                          GSMP
+                    </h1><br>
+                    <h3 class="TTBT">
+                          - Gatherings minecraft smp
+                    </h3><br>
+                    <h3 class="TTBT">
+                         - A server of friends creating content in the game and just hanging out together
+                    </h3>',
+        'post1' => '<h1 class="TTBT">
+                        Hello!
+                    </h1><br>
+                    <h2 class="TTBT">
+                         - You came to my site, which means I have interested you in something.)</h2>
+                    <br><h2 class="TTBT">
+                         - Here you can see my works, find out my contacts or information about other interesting people/companies
+                         that I collaborate with or am a part of!</h2>',
+        'post2'=> '<h1 class="TTBT">
+                        Im currently working on creating a player
+                    </h1><br>
+                    <h2 class="TTBT">
+                        - I didnt find a single good player that worked with operating system functions, like the window grid on Windows 11,
+                         and was functional, customizable, etc., so I decided that I wanted to create something of my own
+                    </h2><br>
+                    <h2 class="TTBT">
+                        - I have a lot of ideas, but I am a web front-end developer and designer, and this is obviously not enough,
+                         which is why I have certain difficulties in implementing them and most likely it will take a long time
+                    </h2><br>
+                    <h2 class="TTBT">
+                        - This is not a commercial project, as soon as the player is finished, I will open access for downloading on this site in the appropriate tab
+                    </h2><br>
+                    <h2 class="TTBT">
+                        - If you have the desire and programming skills, I will be glad to receive any help, write to me in TG, maybe we will
+                         find a common language and together we will be able to quickly implement one of the best players in the world!
+                    </h2>',
+        ],
+    'fr' => [
+        'theme' => 'Changement de thème',
+        'lang'=> 'Langage',
+        'info'=> 'Information',
+        'partfel'=> 'Portefeuille',
+        'partner'=> 'Partenaire',
+        'home'=> 'Maison',
+        'links'=> 'Liens',
+        'hua-chen'=> 'Hua Cheng - Bénédiction des Célestes',
+        'bay-usen'=> 'Bai-Wuxiang - bénédiction des célestes',
+        'THR-Mask'=> '(affiché avec des erreurs non critiques)',
+        'person1'=> '<h2 class="TTBT">
+                         Telona Hyuga Rinne
+                    </h2><br>
+                    <h3 class="TTBT">
+                         - Créateur de contenu Minecraft, programmeur, artiste et simple passionné de technologie
+                    </h3><br>
+                    <h3 class="TTBT">
+                        <cite>
+                         - "Je ne laisse jamais rien de côté pendant longtemps, les choses doivent être mises à jour"
+                        </cite>
+                    </h3><br>
+                        <h3 class="TTBT">
+                         - Aime créer des mécanismes en utilisant de la redstone [Trans-fem <img src="./assets/image/transgender.svg" class="img_1_1" style="width: 1.2vw;">]
+                    </h3>',
+        'Partner1'=> '<h1 class="TTBT">
+                             Minneria-Hyuga
+                        </h1><br>
+                        <h3 class="TTBT">
+                             - L`union de « Empire » et « clan » créant ensemble du contenu pour le jeu Minecraft
+                        </h3><br>
+                        <h3 class="TTBT">
+                             - Le logo symbolise la grande confiance entre les parties
+                        </h3>',
+        'Partner2'=> '<h1 class="TTBT">
+                          GSMP
+                    </h1><br>
+                    <h3 class="TTBT">
+                          - Gatherings minecraft smp
+                    </h3><br>
+                    <h3 class="TTBT">
+                         - Un serveur d`amis créant du contenu dans le jeu et passant simplement du temps ensemble
+                    </h3>',
+        'post1' => '<h1 class="TTBT">
+                        Salut!
+                    </h1><br>
+                    <h2 class="TTBT">
+                         - Vous êtes venu sur mon site, ce qui veut dire que je vous ai intéressé par quelque chose)</h2>
+                    <br><h2 class="TTBT">
+                         - Ici vous pouvez voir mes œuvres, découvrir mes contacts ou des informations sur d`autres personnes/entreprises intéressantes,
+                        avec laquelle je coopère ou dont je suis membre !</h2>',
+        'post2'=> '<h1 class="TTBT">
+                        Je travaille présentement à la création d`un lecteur
+                    </h1><br>
+                    <h2 class="TTBT">
+                        - Je n`ai pas trouvé un seul bon lecteur qui fonctionne avec les fonctions du système d`exploitation, comme la grille de fenêtres sur Windows 11,
+                        et c`était fonctionnel, personnalisable, etc., alors j`ai décidé que je voulais créer quelque chose de mon propre
+                    </h2><br>
+                    <h2 class="TTBT">
+                        - J`ai beaucoup d`idées, mais je suis développeur et concepteur front-end Web, et ce n`est évidemment pas suffisant, c`est
+                         pourquoi j`en ai certainesdes difficultés de mise en œuvre et cela prendra probablement beaucoup de temps
+                    </h2><br>
+                    <h2 class="TTBT">
+                        - Ce n`est pas un projet commercial, dès que le lecteur sera terminé, j`ouvrirai l`accès au téléchargement sur ce site dans l`onglet approprié
+                    </h2><br>
+                    <h2 class="TTBT">
+                        - Si vous avez l`envie et les compétences pour programmer, je serai heureux de vous aider, écrivez-moi en TG, peut-être qu`on trouvera
+                         un langage communet ensemble nous pouvons rapidement mettre en œuvre l`un des meilleurs joueurs au monde!
+                    </h2>',
+        ]
+    ];
+
+?>
