4e73ef012817c4e09471fb4a69c3440ddf929cd5
TelonaHyugaRinne <163213533+TelonaHyuga@users.noreply.github.com>
2025-06-04 11:13:57 +0300

    Update alllanguage.js

--- assets/js/languages/alllanguage.js
+++ assets/js/languages/alllanguage.js
@@ -6,32 +6,32 @@ const langArr = {
     },
 
     "homelink" : {
-        "ru": '<a href="index.html#ru" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3>Главная</h3></li></ul></a>',
-        "en": '<a href="index.html#en" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3>Home</h3></li></ul></a>',
-        "fr": '<a href="index.html#fr" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3>Maison</h3></li></ul></a>',
+        "ru": '<a href="index.html#ru" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3 class="linkh3">Главная</h3></li></ul></a>',
+        "en": '<a href="index.html#en" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3 class="linkh3">Home</h3></li></ul></a>',
+        "fr": '<a href="index.html#fr" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3 class="linkh3">Maison</h3></li></ul></a>',
     },
 
     "inflink" : {
-        "ru": '<a href="info.html#ru" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3>Информация</h3></li></ul></a>',
-        "en": '<a href="info.html#en" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3>Info</h3></li></ul></a>',
-        "fr": '<a href="info.html#fr" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3>Information</h3></li></ul></a>',
+        "ru": '<a href="info.html#ru" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3 class="linkh3">Информация</h3></li></ul></a>',
+        "en": '<a href="info.html#en" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3 class="linkh3">Info</h3></li></ul></a>',
+        "fr": '<a href="info.html#fr" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3 class="linkh3">Information</h3></li></ul></a>',
     },
     
     "partlink" : {
-        "ru": '<a href="partners.html#ru" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3>Партнёры</h3></li></ul></a>',
-        "en": '<a href="partners.html#en" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3>Partners</h3></li></ul></a>',
-        "fr": '<a href="partners.html#fr" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3>Partenaire</h3></li></ul></a>',
+        "ru": '<a href="partners.html#ru" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3 class="linkh3">Партнёры</h3></li></ul></a>',
+        "en": '<a href="partners.html#en" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3 class="linkh3">Partners</h3></li></ul></a>',
+        "fr": '<a href="partners.html#fr" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3 class="linkh3">Partenaire</h3></li></ul></a>',
     },
 
     "partfoliolink" : {
-        "ru": '<a href="partfolio.html#ru" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3>Партфолио</h3></li></ul></a>',
-        "en": '<a href="partfolio.html#en" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3>Portfolio</h3></li></ul></a>',
-        "fr": '<a href="partfolio.html#fr" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3>Portefeuille</h3></li></ul></a>',
+        "ru": '<a href="partfolio.html#ru" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3 class="linkh3">Партфолио</h3></li></ul></a>',
+        "en": '<a href="partfolio.html#en" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3 class="linkh3">Portfolio</h3></li></ul></a>',
+        "fr": '<a href="partfolio.html#fr" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3 class="linkh3">Portefeuille</h3></li></ul></a>',
     },
 
     "linkslink" : {
-        "ru": '<a href="links.html#ru" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3>Ссылки</h3></li></ul></a>',
-        "en": '<a href="links.html#en" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3>Links</h3></li></ul></a>',
-        "fr": '<a href="links.html#fr" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3>Liens</h3></li></ul></a>',
+        "ru": '<a href="links.html#ru" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3 class="linkh3">Ссылки</h3></li></ul></a>',
+        "en": '<a href="links.html#en" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3 class="linkh3">Links</h3></li></ul></a>',
+        "fr": '<a href="links.html#fr" style="width: 13vw;"><ul class="row"><li class="PE"><img src="./assets/image/THRLogo.svg" class="img_1_1 PSVGOFF" style="width: 2vw;"></li><li class="PE"><h3 class="linkh3">Liens</h3></li></ul></a>',
     },
 }
