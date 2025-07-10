02521fb37c05e4cb186613aa66d953bef9f86994
TelonaHyugaRinne <163213533+TelonaHyuga@users.noreply.github.com>
2025-07-10 20:03:54 +0300

    bugfix

---
+++ assets/js/languages/portfellanguage.js
@@ -0,0 +1,22 @@
+const langPortfel = {
+    "hua-chen" : {
+        "ru": "Хуа-Чэн - благословение небожителей",
+        "en": "Hua Cheng - Blessing of the Celestials",
+        "fr": "Hua Cheng - Bénédiction des Célestes",
+    },
+    "bay-usen" : {
+        "ru": "Бай-Усян - благословение небожителей",
+        "en": "Bai-Wuxiang - blessing of the celestials",
+        "fr": "Bai-Wuxiang - bénédiction des célestes",
+    },
+    "THR-Mask" : {
+        "ru": "(отображается с некритичными ошибками)",
+        "en": "(displayed with non-critical errors)",
+        "fr": "(affiché avec des erreurs non critiques)",
+    },
+    "swords" : {
+        "ru": "Мечи",
+        "en": "Swordds",
+        "fr": "Épées",
+    },
+}
