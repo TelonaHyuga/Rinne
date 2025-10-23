6714284979609329ab92bccfe7751fb7f2da4907
TelonaHyugaRinne <telonahyuga@gmail.com>
2025-10-23 03:19:01 +0300

    New design and animations

--- assets/js/languages/pricelanguage.js
+++ assets/js/languages/pricelanguage.js
@@ -11,16 +11,10 @@ const langPrice = {
         "fr": 'Prix: à partir de 1 &#36; WMZ or USDT (dépend de la complexité)',
     },
 
-    "designprice" : {
-        "ru": 'Цена: от 1000 &#8381; руб (63 &#36; WMZ or USDT) (зависит от сложности)',
-        "en": 'Price: from 13 &#36; WMZ or USDT (depends on complexity)',
-        "fr": 'Prix: à partir de 13 &#36; WMZ or USDT (dépend de la complexité)',
-    },
-
     "websiteprice" : {
-        "ru": 'Цена: от 1000 &#8381; руб (13 &#36; WMZ or USDT) (зависит от сложности)',
-        "en": 'Price: from 13 &#36; WMZ or USDT (depends on complexity)',
-        "fr": 'Prix: à partir de 13 &#36; WMZ or USDT (dépend de la complexité)',
+        "ru": 'Цена: от 10000 &#8381; руб (122 &#36; WMZ or USDT) (зависит от сложности)',
+        "en": 'Price: from 122 &#36; WMZ or USDT (depends on complexity)',
+        "fr": 'Prix: à partir de 122 &#36; WMZ or USDT (dépend de la complexité)',
     },
 
     "skin" : {
@@ -35,16 +29,10 @@ const langPrice = {
         "fr": 'Modèle low-poly inspiré de Minecraft. Je me suis récemment consacré à la modélisation, ce qui explique mon petit portfolio. Je fais quand même d`assez bons modèles. Je fais aussi des modèles pour les mods CPM et Figura.<br><br> Temps de travail - dépend de la complexité et est discuté séparément',
     },
 
-    "design" : {
-        "ru": 'Дизайн для сайта, небольшой программы или игры. К сожалению, я не обладаю портфолио и необходимыми навыками программирования, для реализации своих идей, но готова выполнить небольшое вводное задание, после которого вы сможете решить, подходит ли вам мой стиль.<br><br> Время работы - месяц, но в зависимости от сложности может увеличиться',
-        "en": 'Design for a website, a small program or a game. Unfortunately, I do not have a portfolio and the necessary programming skills to implement my ideas, but I am ready to complete a small introductory task, after which you can decide whether my style suits you.<br><br> Time of work - a month, but depending on the complexity it may increase',
-        "fr": 'Conception d`un site web, d`un petit programme ou d`un jeu. Malheureusement, je n`ai pas de portfolio ni les compétences en programmation nécessaires pour concrétiser mes idées, mais je suis prêt à faire une petite introduction, après quoi vous pourrez décider si mon style vous convient.<br><br> Durée du travail : un mois, mais elle peut augmenter selon la complexité',
-    },
-
     "website" : {
-        "ru": 'Небольшой html сайт, от джуниор фронтэендерши. На данный момент, в моём портфолио только 2 сайта, второй сайт вы можете посмотреть в соответствующей вкладке.<br><br> Время работы - от месяца, но в зависимости от сложности может увеличиться',
-        "en": 'A small html site, from a junior front-end developer. At the moment, my portfolio only contains 2 sites, you can see the second site in the corresponding tab.<br><br> Time of work - from a month, but depending on the complexity it can increase',
-        "fr": 'Un p`tit site HTML, créé par un développeur front-end junior. Actuellement, mon portfolio ne contient que deux sites ; vous pouvez consulter le deuxième dans l`onglet correspondant.<br><br> Durée du travail : à partir d`un mois, mais peut augmenter selon la complexité',
+        "ru": 'HTML или PHP сайт, от джуниор фронтэендерши. На данный момент, в моём портфолио только 3 сайта, вы можете посмотреть их в соответствующей вкладке.<br><br> Время работы - от месяца, но в зависимости от сложности может увеличиться',
+        "en": 'An HTML or PHP website, from a junior front-end developer. Currently, my portfolio only contains three websites; you can view them in the corresponding tab.<br><br> Time of work - from a month, but depending on the complexity it can increase',
+        "fr": 'Un site Web HTML ou PHP, réalisé par un développeur front-end junior. Actuellement, mon portfolio ne contient que trois sites Web ; vous pouvez les consulter dans l`onglet correspondant.<br><br> Durée du travail : à partir d`un mois, mais peut augmenter selon la complexité',
     },
 
     "connect" : {
