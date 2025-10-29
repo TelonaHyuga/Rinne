16a5fe9041e5cee8479f138c8f75de92d2cbbbbb
TelonaHyugaRinne <telonahyuga@gmail.com>
2025-10-30 01:49:35 +0300

    Update alllanguage.js

--- assets/js/languages/alllanguage.js
+++ assets/js/languages/alllanguage.js
@@ -6,38 +6,38 @@ const langArr = {
     },
 
     "homelink" : {
-        "ru": '<a href="index.html#ru" style="width: 13vw;"><h3 class="linkh3">&#9775; Главная</h3></a>',
-        "en": '<a href="index.html#en" style="width: 13vw;"><h3 class="linkh3">&#9775; Home</h3></a>',
-        "fr": '<a href="index.html#fr" style="width: 13vw;"><h3 class="linkh3">&#9775; Maison</h3></a>',
+        "ru": '<a href="index#ru" style="width: 13vw;"><h3 class="linkh3">&#9775; Главная</h3></a>',
+        "en": '<a href="index#en" style="width: 13vw;"><h3 class="linkh3">&#9775; Home</h3></a>',
+        "fr": '<a href="index#fr" style="width: 13vw;"><h3 class="linkh3">&#9775; Maison</h3></a>',
     },
 
     "inflink" : {
-        "ru": '<a href="info.html#ru" style="width: 13vw;"><h3 class="linkh3">&#9885; Информация</h3></a>',
-        "en": '<a href="info.html#en" style="width: 13vw;"><h3 class="linkh3">&#9885; Info</h3></a>',
-        "fr": '<a href="info.html#fr" style="width: 13vw;"><h3 class="linkh3">&#9885; Information</h3></a>',
+        "ru": '<a href="info#ru" style="width: 13vw;"><h3 class="linkh3">&#9885; Информация</h3></a>',
+        "en": '<a href="info#en" style="width: 13vw;"><h3 class="linkh3">&#9885; Info</h3></a>',
+        "fr": '<a href="info#fr" style="width: 13vw;"><h3 class="linkh3">&#9885; Information</h3></a>',
     },
     
     "partlink" : {
-        "ru": '<a href="partners.html#ru" style="width: 13vw;"><h3 class="linkh3">&#9884; Партнёры</h3></a>',
-        "en": '<a href="partners.html#en" style="width: 13vw;"><h3 class="linkh3">&#9884; Partners</h3></a>',
-        "fr": '<a href="partners.html#fr" style="width: 13vw;"><h3 class="linkh3">&#9884; Partenaire</h3></a>',
+        "ru": '<a href="partners#ru" style="width: 13vw;"><h3 class="linkh3">&#9884; Партнёры</h3></a>',
+        "en": '<a href="partners#en" style="width: 13vw;"><h3 class="linkh3">&#9884; Partners</h3></a>',
+        "fr": '<a href="partners#fr" style="width: 13vw;"><h3 class="linkh3">&#9884; Partenaire</h3></a>',
     },
 
     "portfoliolink" : {
-        "ru": '<a href="portfolio.html#ru" style="width: 13vw;"><h3 class="linkh3">&#9752; Портфолио</h3></a>',
-        "en": '<a href="portfolio.html#en" style="width: 13vw;"><h3 class="linkh3">&#9752; Portfolio</h3></a>',
-        "fr": '<a href="portfolio.html#fr" style="width: 13vw;"><h3 class="linkh3">&#9752; Portefeuille</h3></a>',
+        "ru": '<a href="portfolio#ru" style="width: 13vw;"><h3 class="linkh3">&#9752; Портфолио</h3></a>',
+        "en": '<a href="portfolio#en" style="width: 13vw;"><h3 class="linkh3">&#9752; Portfolio</h3></a>',
+        "fr": '<a href="portfolio#fr" style="width: 13vw;"><h3 class="linkh3">&#9752; Portefeuille</h3></a>',
     },
 
     "linkslink" : {
-        "ru": '<a href="links.html#ru" style="width: 13vw;"><h3 class="linkh3">&#10002; Ссылки</h3></a>',
-        "en": '<a href="links.html#en" style="width: 13vw;"><h3 class="linkh3">&#10002; Links</h3></a>',
-        "fr": '<a href="links.html#fr" style="width: 13vw;"><h3 class="linkh3">&#10002; Liens</h3></a>',
+        "ru": '<a href="links#ru" style="width: 13vw;"><h3 class="linkh3">&#10002; Ссылки</h3></a>',
+        "en": '<a href="links#en" style="width: 13vw;"><h3 class="linkh3">&#10002; Links</h3></a>',
+        "fr": '<a href="links#fr" style="width: 13vw;"><h3 class="linkh3">&#10002; Liens</h3></a>',
     },
 
     "pricelink" : {
-        "ru": '<a href="price.html#ru" style="width: 13vw;"><h3 class="linkh3">&#36; Цены</h3></a>',
-        "en": '<a href="price.html#en" style="width: 13vw;"><h3 class="linkh3">&#36; Price</h3></a>',
-        "fr": '<a href="price.html#fr" style="width: 13vw;"><h3 class="linkh3">&#36; Prix</h3></a>',
+        "ru": '<a href="price#ru" style="width: 13vw;"><h3 class="linkh3">&#36; Цены</h3></a>',
+        "en": '<a href="price#en" style="width: 13vw;"><h3 class="linkh3">&#36; Price</h3></a>',
+        "fr": '<a href="price#fr" style="width: 13vw;"><h3 class="linkh3">&#36; Prix</h3></a>',
     },
 }
