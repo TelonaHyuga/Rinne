ab0f9e33f21b2696818401efba64b8a5caa08876
TelonaHyugaRinne <telonahyuga@gmail.com>
2025-10-30 01:42:32 +0300

    clean url try 2

--- assets/js/clean.js
+++
@@ -1,5 +0,0 @@
-function clearURLhash() {
-    const url = new URL(window.location);
-    url.hash = ''; // Убираем хэш
-    window.history.pushState({}, '', url); // Обновляем URL без перезагрузки
-}
